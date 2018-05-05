508c2ad6aa46842da417a044d57b00f2daea5a35
Chris Ashworth <chris@cashworth.net>
2018-05-05 22:16:45 +0200

    4.19 update

--- Source/CashUI/CashUI.Build.cs
+++ Source/CashUI/CashUI.Build.cs
@@ -4,8 +4,8 @@ using UnrealBuildTool;
 
 public class CashUI : ModuleRules
 {
-	public CashUI(TargetInfo Target)
-	{
+    public CashUI(ReadOnlyTargetRules Target) : base(Target)
+    {
 		
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "Slate", "SlateCore"  });
 
